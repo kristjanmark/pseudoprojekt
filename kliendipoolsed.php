4e5b106537e9a625b3f22d39837ba14599d5524d
Kristjan <kristjan.mark@khk.ee>
2015-09-28 12:59:29 +0300

    4. Navigeerimatu link [Delivers #104304560]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -1,26 +1,12 @@
 <!doctype html>
-<html lang="et">
+<html lang="ee">
 <head>
     <meta charset="UTF-8">
-    <title>Kliendipoolsed</title>
+    <title>Document</title>
 </head>
 <body>
-<button onclick="myFunction()">Tere, maailm!</button>
-<hr>
-<a href="http://khk.ee" onClick="checkbox()"> Tere, maailm! </a>
-<script>
-    function buttonAlert() {
-        alert("Tere, maailm!");
-    }
-    function checkbox() {
-        var confirmmessage = "Tere, maailm!";
-        var go = "http://khk.ee";
-        if (confirm(confirmmessage)) {
-            window.location = go;
-        } else {
-            alert(message);
-        }
-    }
-</script>
+<button onclick="alert('Tere Maailm!')">Tere Maailm</button><br>
+<a href="http://www.khk.ee" onclick="alert('Tere Maailm');">Tere Maailm</a><br>
+<a href="" onclick="alert('J‰‰me siia!');">J‰‰me Siia</a><br>
 </body>
 </html>
