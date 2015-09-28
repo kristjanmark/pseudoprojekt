8971349d0168cb0c8e794ceaef39f86a3a62b30a
Kristjan <kristjan.mark@khk.ee>
2015-09-28 13:42:05 +0300

    18. Objektid [Delivers #104305290]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,11 +1,7 @@
 <?php
-$isik=array(
-    'eesnimi'=>'Kristjan',
-    'perenimi'=>'Mark',
-    'vanus'=>21
-);
-$eesnimi_algab_vokaaliga = True;
-echo $isik['eesnimi']." ".$isik['perenimi']." (".$isik['vanus'].")<br>";
+$isik=(object) array('eesnimi'=>'Kristjan', 'perenimi'=>'Mark','vanus'=>21,'sugu'=>'Mees');
+$eesnimi_algab_vokaaliga = false;
+echo $isik->{'eesnimi'}." ".$isik->{'perenimi'}." (".$isik->{'vanus'}.") ".$isik->{'sugu'}."<br>";
 if($eesnimi_algab_vokaaliga){
     echo "Nimi algab vokaaliga!";
 }
