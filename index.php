e8c6fc5cc0f7ec4eaee6e0f67e7c45710aec40d0
Kristjan <kristjan.mark@khk.ee>
2015-09-28 13:56:08 +0300

    23. Require [Delivers #104305410]

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
 <!doctype html>
-<html lang="et">
+<html lang="ee">
 <head>
     <meta charset="UTF-8">
-    <title>Projekt1</title>
+    <title>Pseudoprojekt</title>
 </head>
+
 <body>
-<h1>Tere maailm</h1>
-<hr>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias architecto aspernatur, at, corporis dolorum enim
-    et eveniet fuga hic id magnam maxime nemo nihil obcaecati quo repellat repellendus sapiente sunt tempora totam unde
-    voluptate voluptates voluptatibus? Itaque temporibus tenetur veniam? Accusantium adipisci aspernatur atque
-    blanditiis consectetur dicta enim esse et excepturi exercitationem facilis in inventore ipsum itaque labore
-    laudantium minima molestias nam, nesciunt provident quas quod rem sequi sunt tempora temporibus velit! Asperiores
-    cumque ducimus est incidunt iusto libero, neque quidem quod similique temporibus. Adipisci dolore eaque libero nulla
-    quidem? Ad aperiam blanditiis cum delectus, dolorem dolorum ducimus eaque harum illum itaque molestias
-    necessitatibus non nulla provident quaerat quos rem similique sint tenetur ullam, ut velit voluptates! Atque autem
-    delectus eveniet facere harum id inventore officia quas tenetur voluptatibus. Amet dolorem quos rem. Accusantium
-    ducimus eligendi impedit minima reiciendis rerum sint vero. Animi aut doloribus eaque illo laborum nostrum omnis
-    tenetur. Dicta est id minima molestiae, nihil similique vel voluptatum. Cupiditate deleniti, dicta dolore eaque,
-    facere laboriosam magni nihil officiis quasi quibusdam, quidem quis voluptas voluptatum? A architecto consequuntur
-    dolores ea fuga illum impedit itaque magnam maiores, modi necessitatibus nisi non praesentium repudiandae similique,
-    voluptate voluptatum? Blanditiis eligendi maiores molestiae mollitia odit possimus quaerat similique. Illum quam
-    quasi repellendus sed tenetur vel vitae? At blanditiis cumque dolorem eaque eius enim eveniet excepturi id ipsum
-    laborum, minus nulla, placeat quam sapiente sint. Atque, cupiditate explicabo magni odio ratione tempora voluptatem?
-    A animi aperiam architecto consectetur enim et eum eveniet facilis harum illum libero mollitia nemo nobis non omnis
-    quae quaerat quasi, qui quod recusandae reiciendis repellat repudiandae rerum sit, suscipit veritatis vitae
-    voluptatem! Magnam non nulla sapiente sed vel! At dolore hic, iusto natus quaerat sequi veritatis? Eveniet nam
-    obcaecati odio. Dicta dignissimos optio, provident quae quos vitae voluptatum!</p>
+<?php
+$page = isset($_GET['page']) && file_exists($_GET['page'].".php")? $_GET['page'] : 'home';
+require $page.".php";
+?>
+<p>sakfnsoadng</p>
 </body>
+
+
 </html>
