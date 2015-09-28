3124ad2e0db1e3d287df0ec3973429954aadc429
Kristjan <kristjan.mark@khk.ee>
2015-09-28 12:21:26 +0300

    2. Tere maailm button [Delivers #104302998]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,6 +5,11 @@
     <title>Kliendipoolsed</title>
 </head>
 <body>
-
+<button onclick="myFunction()">Tere, maailm!</button>
+<script>
+    function myFunction() {
+        alert("Tere, maailm!");
+    }
+</script>
 </body>
 </html>
