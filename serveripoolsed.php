2b673c42231daf49aeadcf563ad2cd2ec92117a0
Kristjan <kristjan.mark@khk.ee>
2015-09-28 13:45:12 +0300

    20. Floats [Delivers #104305342]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,6 +1,10 @@
 <?php
 $isik=(object) array('eesnimi'=>'Kristjan', 'perenimi'=>'Mark','vanus'=>21,'sugu'=>'Mees');
 $eesnimi_algab_vokaaliga = strstr("AEIOUÕÄÖÜ",$isik->{'eesnimi'}[0]);
+$arv1=1.23;
+$arv2=2.34;
+$summa=$arv1+$arv2;
+echo "Arvude ".$arv1." ja ".$arv2." summa on ".$summa."<br>";
 echo $isik->{'eesnimi'}." ".$isik->{'perenimi'}." (".$isik->{'vanus'}.") ".$isik->{'sugu'}."<br>";
 if($eesnimi_algab_vokaaliga){
     echo "Nimi algab vokaaliga!";
