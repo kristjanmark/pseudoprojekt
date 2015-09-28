9a0780b8c73aa48ef23aaf0829c7dcb4c26600ad
Kristjan <kristjan.mark@khk.ee>
2015-09-28 13:07:13 +0300

    6. jQuery kass koeraks [Delivers #104304946]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
-        document.addEventListener("click",function(){
-            document.getElementById("pic").src="http://weknowyourdreams.com/images/dog/dog-04.jpg";
-        });
+        function muuda(){
+            $("#pic").attr("src","http://weknowyourdreams.com/images/dog/dog-04.jpg");
+        }
     </script>
 </head>
 <body>
@@ -14,6 +15,6 @@
 <a href="http://www.khk.ee" onclick="alert('Tere Maailm');">Tere Maailm</a><br>
 <a href="" onclick="alert('J‰‰me siia!');">J‰‰me Siia</a><br>
 
-<img id="pic" width="200" height="235" src="http://exmoorpet.com/wp-content/uploads/2012/08/cat.png">
+<img id="pic" onclick="muuda()" width="200" height="235" src="http://exmoorpet.com/wp-content/uploads/2012/08/cat.png">
 </body>
 </html>
