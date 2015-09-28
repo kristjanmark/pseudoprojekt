acb74de69053a46b38b267239ba6bce3a5c472ba
Kristjan <kristjan.mark@khk.ee>
2015-09-28 13:43:29 +0300

    19. Funktsioonid [Delivers #104305304]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,6 +1,6 @@
 <?php
 $isik=(object) array('eesnimi'=>'Kristjan', 'perenimi'=>'Mark','vanus'=>21,'sugu'=>'Mees');
-$eesnimi_algab_vokaaliga = false;
+$eesnimi_algab_vokaaliga = strstr("AEIOUÕÄÖÜ",$isik->{'eesnimi'}[0]);
 echo $isik->{'eesnimi'}." ".$isik->{'perenimi'}." (".$isik->{'vanus'}.") ".$isik->{'sugu'}."<br>";
 if($eesnimi_algab_vokaaliga){
     echo "Nimi algab vokaaliga!";
