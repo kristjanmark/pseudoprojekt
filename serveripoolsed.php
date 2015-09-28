1a7a47851019bebc5548cd106c8b7762af70f231
Kristjan <kristjan.mark@khk.ee>
2015-09-28 13:40:17 +0300

    17. Tingimused [Delivers #104305262]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -4,6 +4,9 @@ $isik=array(
     'perenimi'=>'Mark',
     'vanus'=>21
 );
-$eesnimi_algab_vokaaliga=false;
-echo $isik['eesnimi']." ".$isik['perenimi']."(".$isik['vanus'].")";
+$eesnimi_algab_vokaaliga = True;
+echo $isik['eesnimi']." ".$isik['perenimi']." (".$isik['vanus'].")<br>";
+if($eesnimi_algab_vokaaliga){
+    echo "Nimi algab vokaaliga!";
+}
 ?>
