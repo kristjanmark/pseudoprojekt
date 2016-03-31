307e7b8e2b07a418ae1e60a3db95f1696b0b616c
Kristjan <kristjan.mark@khk.ee>
2016-03-31 18:47:29 +0300

    39. Tabeli filtreerimine [Delivers #116702297]

--- klient.php
+++ klient.php
@@ -14,6 +14,11 @@
         <th onclick="Javascript:sortByColumn(this.cellIndex);">Perenimi</th>
         <th onclick="Javascript:sortByColumn(this.cellIndex);">Email</th>
     </tr>
+    <tr>
+        <th><input placeholder="Otsi" onkeyup="Javascript:filter(this,this.parentNode.cellIndex);"/></th>
+        <th><input placeholder="Otsi" onkeyup="Javascript:filter(this,this.parentNode.cellIndex);"/></th>
+        <th><input placeholder="Otsi" onkeyup="Javascript:filter(this,this.parentNode.cellIndex);"/></th>
+    </tr>
 </table>
 <script class="jsbin" src="js/jquery.min.js"></script>
 </script>
@@ -24,17 +29,17 @@
             var j=data[i];
             $('table').append("<tr><td>"+j[0]+"</td><td>"+j[1]+"</td><td>"+j[2]+"</td></tr>");
         }
-    })
+    });
+var table = document.getElementById('tabel');
+var tableData = table.getElementsByTagName('tbody').item(0);
+var rowData = tableData.getElementsByTagName('tr');
 function sortByColumn(data){
     var data=data;
-    var table = document.getElementById('tabel');
-    var tableData = table.getElementsByTagName('tbody').item(0);
-    var rowData = tableData.getElementsByTagName('tr');
-    var getTheader=document.getElementById('tabel').getElementsByTagName('tbody').item(0).getElementsByTagName('tr').item(0).getElementsByTagName('th').item(data)
+    var getTheader=rowData.item(0).getElementsByTagName('th').item(data)
     getTheader.classList.toggle('asc');
     if(getTheader.classList[0]=='asc'){
         for(var i = 0; i < rowData.length - 1; i++){
-            for(var j = 1; j < rowData.length - (i + 1); j++){
+            for(var j = 2; j < rowData.length - (i + 1); j++){
                 if(rowData.item(j).getElementsByTagName('td').item(data).innerHTML > rowData.item(j+1).getElementsByTagName('td').item(data).innerHTML){
                     tableData.insertBefore(rowData.item(j+1),rowData.item(j));
                 }
@@ -43,12 +48,32 @@ function sortByColumn(data){
     }
     else{
         for(var i = 0; i < rowData.length - 1; i++){
-            for(var j = 1; j < rowData.length - (i + 1); j++){
+            for(var j = 2; j < rowData.length - (i + 1); j++){
                 if(rowData.item(j).getElementsByTagName('td').item(data).innerHTML < rowData.item(j+1).getElementsByTagName('td').item(data).innerHTML){
                     tableData.insertBefore(rowData.item(j+1),rowData.item(j));
                 }
             }
         }
     }
+};
+function filter(data,indeks){
+    var search = data.value.toLowerCase().split(" ");
+    var indeks = indeks;
+    console.log(data);
+    for(var i=2;i<rowData.length;i++){
+        rowText=rowData.item(i).getElementsByTagName('td').item(indeks).innerHTML;
+        console.log(indeks);
+        var displayStyle='none';
+        for (var j = 0; j < search.length; j++) {
+            if(rowText.toLowerCase().indexOf(search[j])>=0){
+                displayStyle ='';
+            }
+            else{
+                displayStyle='none';
+                break;
+            }
+        }
+        table.rows[i].style.display = displayStyle;
+    };
 };
 </script>
